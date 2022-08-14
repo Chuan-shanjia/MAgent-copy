bd686dee47b9686d9a13f8e2547f3daf1494b9c2
Chuan-shanjia <pdz847961766@gmail.com>
2022-08-14 11:22:29 +0800

    first commit

--- MAgent/builtin/config/battle.py
+++ MAgent/builtin/config/battle.py
@@ -14,7 +14,7 @@ def get_config(map_size):
     small = cfg.register_agent_type(
         "small",
         {'width': 1, 'length': 1, 'hp': 10, 'speed': 2,
-         'view_range': gw.CircleRange(6), 'attack_range': gw.CircleRange(1.5),
+         'view_range': gw.CircleRange(20), 'attack_range': gw.CircleRange(1.5),
          'damage': 2, 'step_recover': 0.1,
 
          'step_reward': -0.005,  'kill_reward': 5, 'dead_penalty': -0.1, 'attack_penalty': -0.1,
@@ -31,7 +31,5 @@ def get_config(map_size):
     # reward shaping to encourage attack
     cfg.add_reward_rule(gw.Event(a, 'attack', b), receiver=a, value=0.2)    #Event是gridworld中的EventNode类
     cfg.add_reward_rule(gw.Event(b, 'attack', a), receiver=b, value=0.2)
-    cfg.add_reward_rule(gw.Event(a, 'in_a_line'), receiver=a, value=4)
-    cfg.add_reward_rule(gw.Event(b, 'in_a_line'), receiver=b, value=4)
 
     return cfg
