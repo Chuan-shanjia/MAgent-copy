5c500adaaa457389b9761ccde0239dafb784a609
Chuan-shanjia <pdz847961766@gmail.com>
2022-09-01 21:26:45 +0800

    first commit

--- MAgent/builtin/config/battle.py
+++ MAgent/builtin/config/battle.py
@@ -13,12 +13,21 @@ def get_config(map_size):
 
     small = cfg.register_agent_type(
         "small",
-        {'width': 1, 'length': 1, 'hp': 10, 'speed': 2,
-         'view_range': gw.CircleRange(12), 'attack_range': gw.CircleRange(1.5),
-         'damage': 2, 'step_recover': 0.1, 'kill_supply': 2,
-
-         'step_reward': -0.005,  'kill_reward': 10, 'dead_penalty': -0.1, 'attack_penalty': -0.1,
-         })
+        {
+            "width": 1,
+            "length": 1,
+            "hp": 10,
+            "speed": 2,
+            "view_range": gw.CircleRange(6),
+            "attack_range": gw.CircleRange(1.5),
+            "damage": 2,
+            "step_recover": 0.1,
+            "step_reward": -0.005,
+            "kill_reward": 5,
+            "dead_penalty": -0.1,
+            "attack_penalty": -0.1,
+        },
+    )
 
     #small是智能体的属性，可以改
 
@@ -29,7 +38,7 @@ def get_config(map_size):
     b = gw.AgentSymbol(g1, index='any')
 
     # reward shaping to encourage attack
-    cfg.add_reward_rule(gw.Event(a, 'attack', b), receiver=a, value=0.5)    #Event是gridworld中的EventNode类
-    cfg.add_reward_rule(gw.Event(b, 'attack', a), receiver=b, value=0.5)
+    cfg.add_reward_rule(gw.Event(a, 'attack', b), receiver=a, value=0.2)    #Event是gridworld中的EventNode类
+    cfg.add_reward_rule(gw.Event(b, 'attack', a), receiver=b, value=0.2)
 
     return cfg
